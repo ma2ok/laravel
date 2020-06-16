92afd3f7f4b03f375e1767bd06ae9b92a1b7ee8a
Tubasa Matsuoka <workwing0283@gamil.com>
2020-06-16 19:15:31 +0900

    リクエスト追加

--- myblog/app/Http/Controllers/PostsController.php
+++ myblog/app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Http\Requests\PostRequest;
 
 class PostsController extends Controller
 {
@@ -11,30 +12,37 @@ class PostsController extends Controller
       // $posts = \App\Post::all();
       // $posts = Post::all();
       // $posts = Post::orderBy('created_at', 'desc')->get();
-    $posts = Post::latest()->get();
+      $posts = Post::latest()->get();
       // $posts = [];
       // dd($posts->toArray()); // dump die
       // return view('posts.index', ['posts' => $posts]);
-    return view('posts.index')->with('posts', $posts);
+      return view('posts.index')->with('posts', $posts);
     }
 
     // public function show($id) {
     public function show(Post $post) {
       // $post = Post::find($id);
       // $post = Post::findOrFail($id);
-    return view('posts.show')->with('post', $post);
+      return view('posts.show')->with('post', $post);
     }
 
     public function create() {
-    return view('posts.create');
+      return view('posts.create');
     }
 
-    public function store(Request $request) {
-      $this->validate($request, [
-        'title' => 'required|min:3',
-        'body' => 'required'
-      ]);
+    public function store(PostRequest $request) {
       $post = new Post();
+      $post->title = $request->title;
+      $post->body = $request->body;
+      $post->save();
+      return redirect('/');
+    }
+
+    public function edit(Post $post) {
+      return view('posts.edit')->with('post', $post);
+    }
+
+    public function update(PostRequest $request, Post $post) {
       $post->title = $request->title;
       $post->body = $request->body;
       $post->save();
