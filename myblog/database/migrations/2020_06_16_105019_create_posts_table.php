c9515a7f3d2fa0fc8150170d345bb15633e87de1
Tubasa Matsuoka <workwing0283@gamil.com>
2020-06-16 11:23:47 +0900

    修正

--- myblog/database/migrations/2020_06_16_105019_create_posts_table.php
+++ myblog/database/migrations/2020_06_16_105019_create_posts_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreatePostsTable extends Migration
 {
@@ -14,7 +14,7 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title');
             $table->text('body');
             $table->timestamps();
