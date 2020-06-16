786b427b8625bdbd06d97a2d9497cca66d292d5b
Tubasa Matsuoka <workwing0283@gamil.com>
2020-06-16 17:01:36 +0900

    ヘッダー作成

--- myblog/app/Http/Controllers/PostsController.php
+++ myblog/app/Http/Controllers/PostsController.php
@@ -24,4 +24,9 @@ class PostsController extends Controller
       // $post = Post::findOrFail($id);
     return view('posts.show')->with('post', $post);
     }
+
+    public function create() {
+    return view('posts.create');
+    }
+
 }
