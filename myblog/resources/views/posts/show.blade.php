117a22bc5893e81ebd0cfa1f9618adb522f1ea99
Tubasa Matsuoka <workwing0283@gamil.com>
2020-06-17 11:40:25 +0900

    コメント追加機能

--- myblog/resources/views/posts/show.blade.php
+++ myblog/resources/views/posts/show.blade.php
@@ -3,6 +3,32 @@
 @section('title', $post->title)
 
 @section('content')
-<h1>{{ $post->title }}</h1>
+<h1>
+  <a href="{{ url('/') }}" class="header-menu">Back</a>
+  {{ $post->title }}
+</h1>
 <p>{!! nl2br(e($post->body)) !!}</p>
+
+<h2>Comments</h2>
+<ul>
+  @forelse ($post->comments as $comment)
+  <li>
+    {{ $comment->body }}
+  </li>
+  @empty
+  <li>No comments yet</li>
+  @endforelse
+</ul>
+<form method="post" action="{{ action('CommentsController@store', $post) }}">
+  {{ csrf_field() }}
+  <p>
+    <input type="text" name="body" placeholder="enter comment" value="{{ old('body') }}">
+    @if ($errors->has('body'))
+    <span class="error">{{ $errors->first('body') }}</span>
+    @endif
+  </p>
+  <p>
+    <input type="submit" value="Add Comment">
+  </p>
+</form>
 @endsection
